7a54d68a0eb8ab79fc9d9c9665acb4f7a5dc75c1
Jakob Schöttl <jschoett@gmail.com>
2012-04-23 18:00:32 +0200

    Source and project files added

---
+++ Versuche/GT1.C
@@ -0,0 +1,184 @@
+//****************************************************************************
+// @Module        General Purpose Timer Unit 1
+// @Filename      GT1.C
+// @Project       Prakt.dav
+//----------------------------------------------------------------------------
+// @Controller    Infineon C164CI-8E
+//
+// @Compiler      KEIL C166
+//
+// @Codegenerator DAvE 2-0-0
+//
+// @Description   This file contains all functions that use the GPT1 unit.
+//                
+//
+//----------------------------------------------------------------------------
+// @Date          23.04.2012 17:51:39
+//
+//****************************************************************************
+
+
+//****************************************************************************
+// @Project Includes
+//****************************************************************************
+
+#include "MAIN.H"
+
+// USER CODE BEGIN (GT1_General,1)
+
+// USER CODE END
+
+
+//***************************************************************************
+// @Global Variables
+//***************************************************************************
+
+// USER CODE BEGIN (GT1_General,2)
+	static int aTaste = 0xF0, nTaste;
+	static bit tFlag = 0;	// Flag "Alter Status == Neuer Status" der ISR						
+	static bit iCnt = 0;
+// USER CODE END
+
+
+//****************************************************************************
+// @Function      void GT1_vInit(void)
+//
+//----------------------------------------------------------------------------
+// @Description   This function initializes the GPT1 component. It effects all
+//                necessary configurations of the SFR, depending on the selected
+//                operating mode. The configuration determines whether the GPT1
+//                interrupts are to be released, and the priority of the 
+//                released interrupt.
+//
+//----------------------------------------------------------------------------
+// @Returnvalue   none
+//
+//----------------------------------------------------------------------------
+// @Parameters    none
+//
+//----------------------------------------------------------------------------
+// @Date          23.04.2012 17:51:39
+//
+//****************************************************************************
+
+void GT1_vInit(void)
+{
+  /// ------------ Timer 3 Control Register ----------
+  ///  timer 3 works in timer mode
+  ///  prescaler factor is 8
+  ///  timer 3 run bit is reset
+  ///  up/down control bit is reset 
+  ///  external up/down control is disabled
+  T3CON = 0x0000;
+  T3    = 0x0000;  //  load timer 3 register
+
+  /// ---------- Timer 2 Control Register ----------
+  ///  timer 2 works in timer mode
+  ///  prescaler factor is 8
+  ///  up/down control bit is reset 
+  ///  external up/down control is disabled
+  T2CON = 0x0000;
+  T2    = 0x0000;  //  load timer 2 register
+
+  ///  enable timer 2 interrupt
+  ///  timer 2 interrupt priority level(ILVL) = 4
+  ///  timer 2 interrupt group level (GLVL) = 0
+  T2IC = 0x0050;
+
+  /// ---------- Timer 4 Control Register ----------
+  ///  timer 4 works in timer mode
+  ///  prescaler factor is 8
+  ///  timer 4 run bit is reset
+  ///  up/down control bit is reset 
+  ///  external up/down control is disabled
+  T4CON = 0x0000;
+  T4    = 0x0000;  //  load timer 4 register
+
+
+
+  // USER CODE BEGIN (GT1_Init,1)
+
+  // USER CODE END
+
+
+  T2R = 1;         //   set timer 2 run bit 
+}
+
+
+//****************************************************************************
+// @Function      void GT1_viIsrTmr2(void) interrupt T2INT
+//
+//----------------------------------------------------------------------------
+// @Description   This is the interrupt service routine for the GPT1 timer 2.
+//                It is called up when the timer 2 register over or under-
+//                flows or if timer 3 is reloaded with the content of timer 2
+//                (external signal or T3 over/underflow) or if an external
+//                capture event arrives.
+//                
+//                Please note that you have to add application specific
+//                code to this function.
+//
+//----------------------------------------------------------------------------
+// @Returnvalue   none
+//
+//----------------------------------------------------------------------------
+// @Parameters    none
+//
+//----------------------------------------------------------------------------
+// @Date          23.04.2012 17:51:39
+//
+//****************************************************************************
+
+void GT1_viIsrTmr2(void) interrupt T2INT
+{
+  // USER CODE BEGIN (GT1_IsrTmr2,1)
+	
+	nTaste = P1H & 0xF0;
+	if (nTaste == aTaste)
+	{
+		if (iCnt == 0)
+		{
+			iCnt = 1;			
+		}
+		else
+		{
+			iCnt = 0;
+			tFlag = (nTaste != 0xF0);
+		}
+	}
+	else
+	{
+		iCnt = 0;
+	}
+
+	aTaste = nTaste;
+
+  // USER CODE END
+}
+
+
+
+
+// USER CODE BEGIN (GT1_General,3)
+bit KeyDown(void)
+{
+	return tFlag;
+}
+
+char GetKey(void)
+{	/* Variablendeklaration */
+	char iRet = 0;
+	switch(nTaste)
+	{	case 0x70:		iRet = '1';
+						break;
+		case 0xB0:		iRet = '2';
+						break;
+		case 0xD0:		iRet = '3';
+						break;
+		case 0xE0:		iRet = '4';
+						break;	  		
+	}
+	return iRet;
+}
+// USER CODE END
+
---
+++ Versuche/MAIN.C
@@ -0,0 +1,194 @@
+//****************************************************************************
+// @Module        Main
+// @Filename      MAIN.C
+// @Project       Prakt.dav
+//----------------------------------------------------------------------------
+// @Controller    Infineon C164CI-8E
+//
+// @Compiler      KEIL C166
+//
+// @Codegenerator DAvE 2-0-0
+//
+// @Description   This file contains the Project initialization function.
+//
+//----------------------------------------------------------------------------
+// @Date          23.04.2012 17:51:39
+//
+//****************************************************************************
+
+/*
+******************************************************************************
+@Note
+
+
+
+
+******************************************************************************
+*/
+
+//****************************************************************************
+// @Project Includes
+//****************************************************************************
+
+#include "MAIN.H"
+
+// USER CODE BEGIN (Main,0)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// USER CODE END
+
+
+//***************************************************************************
+// @Global Variables
+//***************************************************************************
+
+// USER CODE BEGIN (Main,3)
+
+// USER CODE END
+
+
+//****************************************************************************
+// @Function      void Project_Init(void)
+//
+//----------------------------------------------------------------------------
+// @Description   This function initializes the microcontroller.
+//
+//----------------------------------------------------------------------------
+// @Returnvalue   none
+//
+//----------------------------------------------------------------------------
+// @Parameters    none
+//
+//----------------------------------------------------------------------------
+// @Date          23.04.2012 17:51:39
+//
+//****************************************************************************
+
+void Project_Init(void)
+{
+
+  // USER CODE BEGIN (Project_Init,0)
+
+  // USER CODE END
+
+  /// the following initialization is made by the compiler EDE when you
+  /// import the DAvE project
+
+  /// --- initialization of the SYSCON Register ---
+  ///- 256 words system stack
+  ///- the internal ROM area is mapped to segment 1
+  ///- the segmentation is enabled (CSP is saved/restored during interrupt entry/exit)
+  ///- the internal ROM is disabled: accesses to the ROM area use the external bus
+  ///- the pin #BHE (byte high enable) is enabled
+  ///- the system clock output is disabled
+  ///- the pin #WR acts as #WRL and pin #BHE acts as #WRH
+  ///- the on-chip X-Peripherals are enabled and can be accessed
+  ///- pin #RSTIN is an input only
+  ///- the on-chip oscillator watchdog is disabled
+  ///- latched CS mode; CS signals are latch internally
+
+
+  /// --- initialization of the BUSCON 0-4 and ADRRSEL Registers 1-4 ---
+
+  /// ---------- external bus 0 is enabled ----------
+  ///- 16-bit multiplexed bus
+  ///- memory cycle time control: 15 waitstates
+  ///- read/write delay control: activate signal 1 TCL after falling edge of ALE  
+  ///- chip select mode: address chip select 
+  ///- memory tristate control: 1 waitstate
+  ///- ALE lengthening control: lengthened ALE signal
+
+
+  // initializes the IO ports
+  IO_vInit();
+
+  // initializes the Asynchronous/Synchronous Serial Interface (ASC)
+  ASC_vInit();
+
+  // initializes the General Purpose Timer Unit 1 (GPT1)
+  GT1_vInit();
+
+  // USER CODE BEGIN (Project_Init,1)
+
+  // USER CODE END
+
+  /// globally enable interrupts
+  IEN = 1;
+}
+
+
+//****************************************************************************
+// @Function      void main(void)
+//
+//----------------------------------------------------------------------------
+// @Description   This is the main function.
+//
+//----------------------------------------------------------------------------
+// @Returnvalue   none
+//
+//----------------------------------------------------------------------------
+// @Parameters    none
+//
+//----------------------------------------------------------------------------
+// @Date          23.04.2012 17:51:39
+//
+//****************************************************************************
+
+void main(void)
+{
+  // USER CODE BEGIN (Main,1)
+  int ledState = 0;	// LED Status
+  bit oldState = 0;
+
+  char s[40];
+  // USER CODE END
+
+  Project_Init();
+
+  // USER CODE BEGIN (Main,2)
+	
+	ClrScr();
+	DoPrintZ(1, "Hallo!");
+	DoPrintZ(3, "Hallo Welt!");
+	DoPrintZ(4, "Hallo Jakob______________________");
+	//DoPrintZ(1, " 123    ");//Hallo Raffi");
+
+	while(1)
+	{ 	
+
+		bit state = KeyDown();
+
+		if(!oldState && state)
+		{	switch(GetKey())
+			{	case '1':		//printf("Taste 1 ist gedrückt!!\n\r");
+								ledState ^= 0x80; 
+								break;
+				case '2':		//printf("Taste 2 ist gedrückt!!\n\r");
+								ledState ^= 0x40;
+								break;
+				case '3':		//printf("Taste 3 ist gedrückt!!\n\r");
+								ledState ^= 0x20;
+								break;
+				case '4':		//printf("Taste 4 ist gedrückt!!\n\r");
+								ledState ^= 0x10;
+								break;
+				default:		//printf("Fehler bei der Tastenbestimmung!!\n\r");
+								break;
+			}
+			IO_vWritePort(P1L,ledState);
+
+				/*
+			
+			sprintf(s, "Taste %c ist gedrueckt", GetKey()); // mehr als 20 zeichen -> t sollte nicht mehr dabei sein; ohne fehler
+			DoPrintZ(2, "Hallo Welt!"); // es muesste ohne flackern das zweite angezeigt werden, nichts mit hallo welt.
+			DoPrintZ(2, s);
+			  */
+		}
+		oldState = state;
+	}
+
+  // USER CODE END
+}
+
